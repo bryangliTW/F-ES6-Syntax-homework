5a30071e239fdcc6e8adc6774ff7bd4e2aefa728
Bryan Li <yang.li@Bryans-MacBook-Pro.local>
2020-10-22 17:58:34 +0800

    completed practice 1 & 2 implementation

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,10 @@
-const parseData
+const parseData = (input) => {
+    const {data, column} = input;
+    return data.map((profile) => {
+        let info = {};
+        profile.map((inf, index) => info[column[index].name] = inf);
+        return info;
+    });
+};
+
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,11 @@
-const inject
+const inject = (items, sections) => {
+    sections.sort((a, b) => (a.index > b.index) ? 1 : -1);
+    sections.map((request, index) => {
+        const sectionOne = items.slice(0, request.index + index);
+        const sectionTwo = items.slice(request.index + index);
+        items = [...sectionOne, request.content, ...sectionTwo];
+    });
+    return items;
+}
+
 export { inject };
